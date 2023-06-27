4220f70be932bef70c09f2f809d9a68e9815162b
a hkhan <enchante2012@rediffmail.com>
2023-06-27 15:07:31 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider,} from '@tanstack/react-query'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+
+const queryClient = new QueryClient()
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+    
+
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -29,6 +35,7 @@ function App() {
   <Route path="contact-us" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
+import { useQuery } from  '@tanstack/react-query';
+/*const useShowById = showId => {
+const [showData, setShowData] = useState(null);
+const [showError, setShowError] = useState(null);
+
+useEffect(() => {
+    //   getShowById()
+    async function fetchData() {
+       try {
+           const data = await getShowById(showId);
+           setShowData(data);
+       } catch(err) {
+           setShowError(err);
+       }
+      
+      // console.log(data);
+    }
+
+    fetchData();
+   }, [showId]);
+   return {showData , showError};
+};
+*/
 const Show = () => {
     const  { showId } = useParams();
-    const [showData, setShowData] = useState(null);
-    const [showError, setShowError] = useState(null);
-    useEffect(() => {
-     //   getShowById()
-     async function fetchData() {
-        try {
-            const data = await getShowById(showId);
-            setShowData(data);
-        } catch(err) {
-            setShowError(err);
-        }
-       
-       // console.log(data);
-     }
+  //  const  {showData,showError} = useShowById(showId);
+  const { data : showData , error : showError} = useQuery({
+    queryKey:['show', showId],
+queryFn : () => getShowById(showId),
+});
 
-     fetchData();
-    }, [showId]);
 
     if(showError) {
         return <div>We have an error : {showError.message}</div>
